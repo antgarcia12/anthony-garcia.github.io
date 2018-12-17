95820933bfd510974380034fc204d3f0e5fdd199
Anthony Garcia <garcia.anthony577@gmail.com>
2018-12-16 20:52:39 -0700

    adding personal website to github

---
+++ files/my-pwp-anthony/public_html/documentation/milestone-1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Personal Website Project</title>
+	</head>
+	<body>
+		<h1>Purpose, Audience, Goal</h1>
+		<h3>Purpose: </h3>
+			<p>The purpose of my personal website is to showcase the technologies I've learned at Deep Dive to begin my way into the local business scene that Albuquerque has to offer.</p>
+		<h3>Audience: </h3>
+			<p>The audience that I plan to reach out to would be mainly clients that have local or start up businesses, especially the clients that are trying to put themselves out there any way they can, and in my case that would be a website I create for them.</p>
+		<h3>Goal: </h3>
+			<p>In the end, my goal is to attract freelance clients, from a wide range of local business, of any type, that could potentially be lasting partnerships.</p>
+		<h1>Persona</h1>
+			<p><strong>Name: </strong>Sandy Sanchez</p>
+			<p><strong>Age: </strong>30 years old</p>
+			<p><strong>Job:</strong>Nursing, RN</p>
+			<p><strong>Technology: </strong><em>Computer- </em>HP EliteBook x360 1020 Laptop OS: Windows 10 Pro <em>Phone- </em> iPhone X with iOS 12</p>
+			<p><strong>Attitude: </strong>Sandy is a passionate nurse, enjoying what she does everyday, taking care of people on a daily basis. Recently, Sandy had been discussing with her father about a new venture into the house flipping business, as she has the money to do so. Sandy is certainly enthusiastic on what could become of this new challenge.</p>
+			<p><strong>Behavior:</strong>Sandy understand that the task of starting the business is going to be tough, especially trying to do so as nurse, but she loves to multitask. She has the advantage as her father once held a house flipping business when he was younger. One thing that her father wanted to make her aware of was that we live in a very technological era, where everything we look for can be found on the internet. Sandy has the advantage of the internet, as her father didn't have access to such a useful tool when he was younger. It begins to frustrate Sandy because she doesn't know where to begin to promote her business, besides getting in contact with people. She rather show people that her family has experience in flipping houses rather than explaining over email, phone, or in person with the chance of getting no business, as time is very crucial in her life.</p>
+			<p><strong>Goal: </strong>Sandy wants the goal of creating a website that shows the necessary information for a client to decide whether or not they want her business. She stumbles upon my website and reads that I create websites with the idea of helping local businesses start up or expand on their mission statements.</p>
+		<h4>Frustration: </h4>
+		<ul>
+			<li>Not having enough time for both jobs.</li>
+			<li>A chance of being disorganized.</li>
+			<li>Unable to find houses to flip.</li>
+			<li>Working with a developer who has less than 1 year of experience.</li>
+			<li>Trying to find clients who are looking to flip a house. </li>
+		</ul>
+	</body>
+</html>
---
+++ files/my-pwp-anthony/public_html/index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Anthony' Website</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+
+
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+		<!-- Mailer -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/jquery-validator.js"></script>
+
+
+		<!-- Font Awesome -->
+		<script defer src="https://use.fontawesome.com/releases/v5.5.0/js/all.js" integrity="sha384-GqVMZRt5Gn7tB9D9q7ONtcp4gtHIUEW/yG7h98J7IpE3kpi+srfFyyB/04OV6pG0" crossorigin="anonymous"></script>
+
+		<!-- Google Font -->
+		<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+
+		<!-- CSS Page -->
+		<link href="stylesheet.css" rel="stylesheet">
+
+		<!-- Javascript -->
+		<script type="text/javascript" src="./js/button.js"></script>
+	</head>
+	<body>
+		<!-- Nav Bar -->
+		<nav class="navbar navbar-expand-md navbar-dark">
+			<img src="images/zia-bw.jpg" id="zia">
+		</nav>
+		<!-- Welcome -->
+		<header class="h-100" id="welcome">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-12">
+					<h1 class="garcia display-3">Anthony Garcia</h1>
+					<h1 class="web display-4">Web Developer</h1>
+				<div class="about-link col-md-12 col-lg text-center">
+					<a href="#anthony"><button type="button" class="btn btn-outline-light">About</button></a>
+				</div>
+				<div class="portfolio-link col-md-12 col-lg text-center">
+					<a href="#work"><button type="button" class="btn btn-outline-light">Portfolio</button></a>
+				</div>
+				<div class="contact-link col-md-12 col-lg text-center">
+					<a href="#hey"><button type="button" class="btn btn-outline-light">Contact Me</button></a>
+				</div>
+			</div>
+		</div>
+		</header>
+		<!-- About -->
+		<section id="anthony">
+			<div class="container-fluid about-me">
+				<div class="row about">
+					<div class="col-lg-12">
+						<h1 class="display-3 abt-me">About Me</h1>
+						<p class="story">My name is Anthony Garcia. I am a graduated of the CNM Ingenuity's Deep Diving Coding and earned my certification through the Fullstack Web Development bootcamp they offer. Through my experience of the bootcamp, I learned that many web developers' journeys are all different; it showed me the meaning of diversity in a real work-related environment. I was originally going to school to earn my associates in nursing. Through a process of trial and error, I found that nursing was not the right career for me. I didn't want to waste years trying to find a new career and thankfully, a good friend of mine had mention the bootcamp, which drew my attention to what could come. With research and the help of a "gut feeling," I made the choice to take the first step into the world of coding. In all honesty, I had no prior experience to the bootcamp. The weeks went by and I started to see that I didn't experience going into this. I was fortunate enough to learn years of schooling into 10 of the fastest weeks of my life. I am confident in my capabilities and the product I can produce. My goal is to meet anybody's standards, no matter the challenge, any we can work things out, I hope to change the landscape of our community and economy here in Albuquerque.</p>
+					</div>
+					<div class="col-lg-6 mr-1">
+						<img src="images/dd-coding-icon-cnmii-stem.png" class="dd-fluid">
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Portfolio -->
+		<section id="work">
+			<h1 class="display-2 text-align-right port-section">Portfolio</h1>
+			<div class="container mt-5 port-work">
+				<div class="row" id="card-train">
+					<div class="card border-dark col-lg-3 col-sm-6">
+						<img class="img-fluid d-block mx-auto mb-4" src="images/family-photo-stock.jpg" alt="FamilyConn">
+						<div class="card mb-4">
+							<div class="card-body">
+								<h5 class="card-title">FamilyConnect</h5>
+								<p class="card-text">With the help of my capstone team, I was able to create a family platform that help engage and keep in touch of family, both immediate and distant. Although it may be another social media, this app had a goal to bring families closer and share experiences with loved ones.</p>
+							</div>
+						</div>
+					</div>
+					<div class="card border-dark col-lg-3 col-sm-6">
+						<img class="img-fluid d-block mx-auto mb-4" src="images/code-stock.jpg" alt="Code Image">
+						<div class="card mb-4">
+							<div class="card-body">
+								<h5 class="card-title">Data-Design</h5>
+								<p class="card-text">My data design project introduce me to the concepts and the languages of the back end development. I was able to create a data base and add some security to it in order to keep the data safe. It showed me that the back end development is just as important than the front end. </p>
+							</div>
+						</div>
+					</div>
+					<div class="card border-dark col-lg-3 col-sm-6">
+						<img class="img-fluid d-block mx-auto mb-4" src="images/doors-of-oppurtunity.jpg" alt="Opportunity">
+						<div class="card mb-4">
+							<div class="card-body">
+								<h5 class="card-title">Future Goals</h5>
+								<p class="card-text">As my time at Deep Dive ended, I realize I still have a lifetime to learn new concepts in the world of coding. My small experience does not define how driven I am to learn, and hopefully soon, I can add more to my portfolio. Follow me on this journey, I would love to show you what I can do.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Contact -->
+		<section class="contact-section" id="hey">
+		<form id="contact-form" method="POST" action="php/mailer.php" novalidate="novalidate">
+			<div class="fluid-container main-container">
+				<div class="col-md-5 m-auto">
+					<h1 class="display-3" id="cm">Contact Me</h1>
+		<div class="form-group">
+			<label for="name">
+				Name
+			</label>
+			<div class="input-group">
+				<input type="text" class="form-control" id="name" name="name" placeholder="Your Name">
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="email">
+				Email
+			</label>
+			<div class="input-group">
+				<input type="text" class="form-control" id="email" name="email" placeholder="Your Email">
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="message">
+				Message
+			</label>
+			<div class="input-group">
+				<textarea class="form-control" rows="5" id="message" name="message" placeholder="Your Message"></textarea>
+			</div>
+		</div>
+			<div class="g-recaptcha" data-sitekey="6Lek0HsUAAAAAEjr4NxRbvQlVkRG7Vr-p5tdJviY"></div>
+			<button class="btn btn-dark" type="submit">Send</button>
+			<button class="btn btn-danger" type="reset">Reset</button>
+			</div>
+			</div>
+		</form>
+		<!-- End Contact Form -->
+		<div id="output-area"></div>
+		</section>
+		<!-- Footer -->
+		<footer>
+			<div class="container-fluid">
+				<div class="row d-flex display-inline justify-content-center pl-0">
+					<a href="https://github.com/antgarcia12/" target="_blank"><i class="fab fa-github fa-3x"></i></a>
+					<a href="https://www.linkedin.com/in/antgarcia47/" target="_blank"><i class="fab fa-linkedin fa-3x"></i></a>
+				</div>
+			</div>
+		</footer>
+		<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+	</body>
+</html>
